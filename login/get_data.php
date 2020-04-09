b42ebf1dbbc2ea24e44d7bea26e137f7f3060a57
EagleCoder404 <39628234+EagleCoder404@users.noreply.github.com>
2020-04-09 15:13:44 +0530

    added comments

--- login/get_data.php
+++ login/get_data.php
@@ -1,4 +1,8 @@
 <?php
+
+//$_POST stores the input POST data
+//$_GET stores the input GET
+//$_FILE stores input files
 echo "USERNAME:".$_POST['username'];
 echo "PASSWORD:".$_POST['password'];
 ?>
